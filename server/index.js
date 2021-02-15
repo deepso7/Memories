c55e5760770db7cc37fd0814c5b57c88b7e63f75
deeps <deeps.o7o8.y@gmail.com>
2021-02-15 19:29:30 +0530

    Initial Commit

---
+++ server/index.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+import express from "express";
+import mongoose from "mongoose";
+
+const app = express();
+
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
