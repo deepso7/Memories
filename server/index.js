7e3b2ba208099fe11f2c163abaee3180eab38d9e
deeps <deeps.o7o8.y@gmail.com>
2021-02-17 01:18:49 +0530

    Set Frontend

--- server/index.js
+++ server/index.js
@@ -1,8 +1,21 @@
-require("dotenv").config();
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
-import mongoose from "mongoose";
+
+import postRoutes from "./routes/posts.js";
+
+import connectDB from "./config/db.js";
 
 const app = express();
 
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
+
+//Connect Database
+connectDB();
+
+//Define Routes
+app.use("/posts", postRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server is running on port:", PORT));
